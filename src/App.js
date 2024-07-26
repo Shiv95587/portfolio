6c34f4d690fd13bd56a56f9e7562c77dc828cf73
Shiv Kumar <shiv95587@gmail.com>
2024-07-26 17:23:52 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profilePic from "./profile-pic.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="header">
+        <div className="container">
+          <nav className="navbar">
+            <div className="nav-elements">
+              <div className="left">
+                <a href="#" className="nav-element">
+                  Home
+                </a>
+                <a href="#" className="nav-element">
+                  About Me
+                </a>
+                <a href="#" className="nav-element">
+                  Services
+                </a>
+              </div>
+
+              <div className="profile-wrapper">
+                <img src={profilePic} className="profile-pic" alt="Profile" />
+              </div>
+              <div className="right">
+                <a href="#" className="nav-element">
+                  Projects
+                </a>
+                <a href="#" className="nav-element">
+                  Achievements
+                </a>
+                <a href="#" className="nav-element">
+                  Contact
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <section className="hero-section">
+          <div className="container">
+            <div className="hero-section-left">
+              <h3 className="greeting">Hi I am</h3>
+              <h3 className="name">Shiv Kumar</h3>
+              <h1 className="role">Full Stack Developer / ML Engineer</h1>
+              <h5 className="description">
+                I'm Shiv Kumar, a computer science student at FAST University
+                Karachi. I have a strong passion for machine learning, deep
+                learning, computer vision, and web development. Through my
+                academic journey and personal projects, I continuously strive to
+                enhance my skills and contribute to innovative technological
+                solutions.
+              </h5>
+            </div>
+            <div className="hero-section-right"></div>
+          </div>
+        </section>
       </header>
     </div>
   );
